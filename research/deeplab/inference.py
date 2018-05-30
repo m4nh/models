49d1c7555333e973fb372d48d3c4bc7473c7f2cd
pierlui92 <zamapierluigi@hotmail.it>
2018-05-30 15:57:58 +0200

    update inference.py

--- research/deeplab/inference.py
+++ research/deeplab/inference.py
@@ -1,23 +1,3 @@
-# Copyright 2018 The TensorFlow Authors All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Segmentation results visualization on a given set of images.
-
-See model.py for more details and usage.
-"""
-
 import math
 import os.path
 import time
@@ -96,7 +76,7 @@ class inference(object):
 
 def main(unused_argv):
     #### load npy rgb ####
-    img = np.load("/home/pier/Desktop/aaa.npy")
+    img = np.load("/home/pier/Desktop/rgb.npy")
     with tf.Session() as sess:
         model=inference(sess)
         #### optional save_path, full path to output ####
